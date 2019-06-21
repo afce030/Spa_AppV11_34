215de5bfbbce01cd5064dd44263c440f2fad70d5
afce030 <andrescardona056@hotmail.com>
2019-06-21 16:30:32 -0500

    libreria no utilizada 2

--- app/src/main/java/com/example/spa_appv11_34/AjustesActivity.java
+++ app/src/main/java/com/example/spa_appv11_34/AjustesActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
