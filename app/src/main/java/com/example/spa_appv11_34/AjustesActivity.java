67a88be4190ef3ff500e1805c0e94e056a6ac359
afce030 <andrescardona056@hotmail.com>
2019-06-21 16:29:39 -0500

    libreria no utilizada

--- app/src/main/java/com/example/spa_appv11_34/AjustesActivity.java
+++ app/src/main/java/com/example/spa_appv11_34/AjustesActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
